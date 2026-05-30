bde73ca9e6ae5a1113a889a66f66e12c4eae42b7
gugapriya <gugavivek@icloud.com>
2026-05-30 12:19:53 -0700

    two changes in comment lines

--- webuitesting/E2ETesting.spec.js
+++ webuitesting/E2ETesting.spec.js
@@ -4,6 +4,7 @@ const {POManager}=require('../pageobjects/POManager');
 const testdata=JSON.parse(JSON.stringify(require('./Utils/TestData.json')));
 for( const eachdata  of testdata)
 {
+    //uSES PAGEOBJECT MODEL
 test.only(`E2E Testing ${eachdata.shippingcountry}`,async({page})=>
     {
     console.log('Passing to POManager:', eachdata);
